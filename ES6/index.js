c6b745957f7a972180e8b0becb4b3b67040c1c18
Sebastian Reichenbach <reichenbach.s@gmail.com>
2021-02-11 21:01:47 +0100

    seb, initial commit

---
+++ ES6/index.js
@@ -0,0 +1,21 @@
+
+
+// VANILLA JS
+var getBook = function(){
+    var title = `1984`;
+    var author = `G. Orwell`;
+    console.log(`Title: ` + title + ` Author: ` + author);
+    
+}
+
+getBook();
+
+// ES6
+
+const getBook = () => {
+    const title = `1984`;
+    const author = `G. Orwell`;
+    console.log(`Title: ${title} Author: ${author}`);
+}
+
+getBook();
