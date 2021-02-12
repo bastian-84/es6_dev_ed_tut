0577e897dc1b8a95093a0eb9f67d6385594fc5ce
Sebastian Reichenbach <reichenbach.s@gmail.com>
2021-02-12 14:31:53 +0100

    seb, finished tut

--- ES6/index.js
+++ ES6/index.js
@@ -1,21 +1,19 @@
 
 
 // VANILLA JS
-var getBook = function(){
-    var title = `1984`;
-    var author = `G. Orwell`;
-    console.log(`Title: ` + title + ` Author: ` + author);
-    
-}
 
-getBook();
 
 // ES6
 
-const getBook = () => {
-    const title = `1984`;
-    const author = `G. Orwell`;
-    console.log(`Title: ${title} Author: ${author}`);
-}
+const promise = new Promise( (resolve, reject)  => {
+    // Here is a sync
+    setTimeout( () => {
+        reject( new Error("Something went wrong"));
 
-getBook();
+    }), 2000;
+});
+
+promise.then( (data) => {
+    console.log(data);
+})
+.catch( (err) => console.log(err));
